b17b709dd155c5a5a4d1233bd5d99c52406bcc92
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-22 17:45:01 +0100

    making list with email already registered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-var mail_utente = prompt;
+// VARIABle
+var register = [
+  "gio90@gmail.com",
+  "stefy@gmail.com",
+  "adriana@gmail.com",
+  "bibi@gmail.com",
+  "martina@gmail.com",
+];
+var autorized = false;
+
+// Asking email to user
+var user_mail = prompt("Enter your email?");
+// verify if th email is already registered
+for(var i = 0; i < registerd .length; i++){
+    var registered = registered [i];
+    if(user_mail)
