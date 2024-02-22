4b65e0fa364210cc3c415949a3b9eb9dcd3466e1
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-22 18:13:42 +0100

    giving the output for authorization, displaying welcome msg if registered the register otherwise will display not authorizedmsg

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// VARIABle
+// VARIABLE
 var register = [
   "gio90@gmail.com",
   "stefy@gmail.com",
@@ -6,17 +6,28 @@ var register = [
   "bibi@gmail.com",
   "martina@gmail.com",
 ];
-var autorized = false; //will change after
+var authorized = false; // Set to false because it will change after*
 
 // Asking email to user
 var user_mail = prompt("Enter your email?");
-// verify if th email is already registered
-for (var i = 0; i < registerd.length; i++) {
-  //loop
-  var registered = registered[i]; //Array
+// verify if the email is already registered
+for (var i = 0; i < register.length; i++) {
+  // loop
+  var registered = register[i]; // Array
   if (user_mail === registered) {
     // user's input
     authorized = true;
-    break; //stopping the loop with true
+    break; // stopping the loop with true*
   }
+}
+
+// Display output based on authorization
+if (authorized) {
+  // If authorized, display a welcome message with the user's email
+  document.getElementById("output").innerHTML = "Welcome " + user_mail + "!";
+} else {
+  // If not authorized, display a message indicating no access and a suggestion to register
+  document.getElementById("output").innerHTML =
+    "IMPOSSIBLE TO ACCESS IF NOT REGISTERED!" +
+    "<br>Please register to access the contents of the page";
 }
