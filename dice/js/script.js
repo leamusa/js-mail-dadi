64c9fed4a4437eb811ce4ebaa65ca55f272e0501
leandramusto@gmail.com <leandramusto@gmail.com>
2024-02-26 01:52:57 +0100

    for cycle

--- dice/js/script.js
+++ dice/js/script.js
@@ -1,35 +1,30 @@
-// End of 1 ex
-//Second ex: Dice Game
-//  Generate a random number from 1 to 6, (both) for the player and for the computer.
-// Determine the winner, based on who makes the highest score.
-
-//  1- asking to the player to put the name
-
+// 1 - asking the player to put their name
 var nome = prompt("Put here your name");
 console.log(nome);
 
 document.getElementById("user").innerHTML = nome;
-1;
-// 2 - generare un numero random per il giocatore
 
+// 2 - generate a random number for the player
 var numRandomUser = Math.floor(Math.random() * 6) + 1;
 console.log(numRandomUser);
 
 document.getElementById("rolling-user").innerHTML = numRandomUser;
-// 3 - generating a random number for pc
 
-var numRandomPc = Math.floor(Math.random() * 6) + 1;
-console.log(numRandomPc);
+// 3 - generate a random number for the computer using a loop
+for (var i = 0; i < 1; i++) {
+  var numRandomPc = Math.floor(Math.random() * 6) + 1;
+  console.log(numRandomPc);
 
-document.getElementById("rolling-pc").innerHTML = numRandomPc;
-// 4 - declaring who is the winner based to the higher point
+  document.getElementById("rolling-pc").innerHTML = numRandomPc;
+}
 
+// 4 - declare who is the winner based on the higher point
 var result = "Congratulations... You won!!!!";
 
-(" Congratulations... You won!!!!");
-
 if (numRandomUser < numRandomPc) {
-  result = "Sorry...You lost!";
-} else if (numRandomUser == numRandomPc) {
-  result = "draw";
+  result = "Sorry... You lost!";
+} else if (numRandomUser === numRandomPc) {
+  result = "It's a draw!";
 }
+
+console.log(result);
